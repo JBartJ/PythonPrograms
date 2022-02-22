09ae75a17d31d4eeb46dfa102f3329635c8b86dd
JBartJ <utobi31@gmail.com>
2022-02-22 21:43:32 +0100

    DynamicDictionary program added

--- DynamicDictionary.py
+++ DynamicDictionary.py
@@ -1,10 +1,3 @@
-'''
-A program that will allow the user
-1) Add new definitions
-2) Look for existing definitions
-3) Delete definitions chosen by him
-'''
-
 dictionary = {}
 
 end = False
---
+++ README.md
@@ -0,0 +1,5 @@
+# DynamicDictionary
+A program that will allow the user:
+1) Add new definitions
+2) Look for existing definitions
+3) Delete definitions chosen by him
